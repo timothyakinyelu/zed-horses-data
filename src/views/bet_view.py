d2e50c1c719dc6993c81030eb3fc5d83845f496f
loverofMush <tolujamal1@gmail.com>
2021-05-23 16:47:40 +0100

    -modified views

--- src/views/bet_view.py
+++ src/views/bet_view.py
@@ -1,9 +1,17 @@
+from flask import render_template
 from flask.views import MethodView
 from src.resolvers import DataResolver
 from . import bet
 
 
 class IndexView(MethodView):
+    """ run graphql playground locally """
+    
+    def get(self):
+        return render_template('inputDate.html') 
+
+
+class GetDataView(MethodView):
     """ run graphql playground locally """
     
     def get(self):
@@ -20,12 +28,17 @@ class ReadDataView(MethodView):
 
 # name routes
 index_view = IndexView.as_view('index_view')
+get_data_view = GetDataView.as_view('get_data_view')
 read_data_view = ReadDataView.as_view('read_data_view')
 
 # url rule
 bet.add_url_rule(
     '/',
     view_func=index_view
+)
+bet.add_url_rule(
+    '/get-data',
+    view_func=get_data_view
 )
 bet.add_url_rule(
     '/read-data',
